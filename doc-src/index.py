0b87bdbedd2cb38fe43fd20b7d8c69f2af272773
Aldo Cortesi <aldo@nullcube.com>
2009-03-23 13:54:36 +1100

    Documentation now in RST.

--- doc-src/index.py
+++ doc-src/index.py
@@ -1,22 +1,16 @@
 import countershape.layout
+import countershape.template
 import countershape.widgets
 import countershape.grok
 from countershape.doc import *
 
-class CSLayout(countershape.layout.Layout):
-    components = ["pageTitle", "body", "header"]
-    def __init__(self):
-        countershape.layout.Layout.__init__(
-            self,
-            Template(False, readFrom("_layout.html"))
-        )
-this.layout = CSLayout()
-
+this.layout = countershape.layout.Layout("_layout.html")
+this.markdown = "rst"
 ns.docTitle = "Countershape Manual"
 ns.docMaintainer = "Aldo Cortesi"
 ns.docMaintainerEmail = "aldo@nullcube.com"
 ns.foot = "Copyright Nullcube 2007"
-ns.head = readFrom("_header.html")
+ns.head = countershape.template.File(None, "_header.html")
 ns.sidebar = countershape.widgets.SiblingPageIndex(
             '/index.html', exclude=['countershape']
           )
