3ace426d8d5bdaff92c80a72292d8361d8ef1c04
Samiuela LV Taufa <samt@employersmutual.com.au>
2009-08-18 13:57:23 +1000

    API documentation update

--- doc-src/api/index.py
+++ doc-src/api/index.py
@@ -1,10 +1,16 @@
 from countershape.doc import *
 
 pages = [
+    Page("blog.html", "blog"),
     Page("doc.html", "doc"),
     Page("grok.html", "grok"),
     Page("html.html", "html"),
     Page("layout.html", "layout"),
     Page("model.html", "model"),
+    Page("rssgen.html", "rssgen"),
+    Page("state.html", "state"),
     Page("template.html", "template"),
+    Page("textish.html", "textish"),
+    Page("utils.html", "utils"),
+    Page("widgets.html", "widgets"),
 ]
