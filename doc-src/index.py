6ed1a5a2684d31c721b3e3c1d05540a81eae4bb6
Samiuela LV Taufa <samt@employersmutual.com.au>
2009-08-18 13:58:39 +1000

    Increase documentation (for better or worse)
      - model.py - more whitespace makes documentation
        work better
      - doc.html - some information on how automated
        code documentation might work
      - index.html - copy content from README
      - doc.html (use instead of code.html) + highlight
        some points where countershape can be used
        for automated document generation
      - api/apiref.html - updated Reference documentation
        for APIs

--- doc-src/index.py
+++ doc-src/index.py
@@ -9,7 +9,7 @@ ns.docTitle = "Countershape Manual"
 ns.docMaintainer = "Aldo Cortesi"
 ns.docMaintainerEmail = "dev@nullcube.com"
 ns.copyright = "Copyright Nullcube 2007"
-ns.head = countershape.template.File(None, "_header.html")
+ns.head = countershape.template.File(None, "_banner.html")
 ns.sidebar = countershape.widgets.SiblingPageIndex(
             '/index.html', 
             exclude=['countershape']
@@ -19,8 +19,8 @@ ns.parse = countershape.grok.parse("../countershape")
 pages = [
     Page("index.html", "Introduction"),
     Page("structure.html", "Document Structure"),
-    Page("code.html", "Documenting Code"),
-    Page("api.html", "API Reference"),
+    Page("doc.html", "Documenting Code"),
+    Page("api/apiref.html", "API Reference"),
     Directory("api"),
     PythonModule("../countershape", "Source"),
     Page("admin.html", "Administrivia")
