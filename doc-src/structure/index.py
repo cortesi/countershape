371e08013c63c4e30cb6424246fb415db21a2991
Samiuela LV Taufa <samt@employersmutual.com.au>
2009-11-05 14:54:27 +1100

    Initial Structure Discussion, add more information about Textish

---
+++ doc-src/structure/index.py
@@ -0,0 +1,8 @@
+from countershape.doc import *
+this.markdown = "textish"
+
+ns.docTitle = "Document Structure"
+pages = [
+    Page("textish/textish.html", "Textish"),
+    Directory("textish"),
+]
---
+++ doc-src/structure/textish/index.py
@@ -0,0 +1,6 @@
+from countershape.doc import *
+this.markdown = "rst"
+pages = [
+    Page("character.html", "Character"),
+    Page("corner.html", "Corner"),
+]
