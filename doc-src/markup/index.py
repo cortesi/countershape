7bf339c2eb25a7fcecb11be8837ed76fc9b00ca0
Samiuela LV Taufa <samt@nomoa.com>
2011-05-23 12:57:24 +1000

    Remove "textish" as no longer supported markup system.

--- doc-src/markup/index.py
+++ doc-src/markup/index.py
@@ -13,13 +13,6 @@ pages = [
                 title="reStructuredText",
                 pageTitle = "reStructuredText"
             ),           
-            Directory("rest"),
-            
-            Page(
-                "textish/textish.html",
-                title="Textish",
-                pageTitle = "Mark Up with MarkDown"
-            ),
-            Directory("textish"),
+            Directory("rest"),            
         ]
         
